985f6ce61cabe093d4b0f95031a7df5936d795c0
Rasoir-genereux <kevintonou@icloud.com>
2025-09-23 18:25:59 -0400

    TP2 Exo 1

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,17 +7,25 @@ from app.main import app
 async def test_predict_success():
     async with AsyncClient(app=app, base_url="http://test") as client:
         resp = await client.post("/predict", json={
-        "features": [3.5, 1.2, 4.9]
+        "features": [0.5, 1, 1.5]
     })
     assert resp.status_code == 200
-    assert {"predictions": [7.0, 2.4, 9.8]} == resp.json()
+    assert {"predictions": [1, 2, 3]} == resp.json()
+
+@pytest.mark.anyio
+async def test_predict_failure():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        resp = await client.post("/predict", json={
+        "features": [0.5, 1, 1.5]
+    })
+    assert resp.status_code == 200
+    assert {"predictions": [1, 4, 3]} == resp.json()
 
 @pytest.mark.anyio
 async def test_predict_unprocessable_entity():
     async with AsyncClient(app=app, base_url="http://test") as client:
         resp = await client.post("/predict", json={
         "feature1": 3.5,
-        "feature2": 1.2,
-        "feature3": 4.9
+        "feature2": 1.2
     })
     assert resp.status_code == 422
