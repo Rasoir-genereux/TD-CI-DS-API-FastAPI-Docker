6480b4038211803c4f2a62baac027bb97df5ae02
Rasoir-genereux <kevintonou@icloud.com>
2025-09-23 18:40:42 -0400

    Test Exo 1

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,15 +13,6 @@ async def test_predict_success():
     assert {"predictions": [1, 2, 3]} == resp.json()
 
 @pytest.mark.anyio
-async def test_predict_failure():
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        resp = await client.post("/predict", json={
-        "features": [0.5, 1, 1.5]
-    })
-    assert resp.status_code == 200
-    assert {"predictions": [1, 4, 3]} == resp.json()
-
-@pytest.mark.anyio
 async def test_predict_unprocessable_entity():
     async with AsyncClient(app=app, base_url="http://test") as client:
         resp = await client.post("/predict", json={
@@ -29,3 +20,12 @@ async def test_predict_unprocessable_entity():
         "feature2": 1.2
     })
     assert resp.status_code == 422
+
+@pytest.mark.anyio
+async def test_predict_failure():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        resp = await client.post("/predict", json={
+        "features": [0.5, 1, 1.5]
+    })
+    assert resp.status_code == 200
+    assert {"predictions": [1, 4, 3]} == resp.json()
